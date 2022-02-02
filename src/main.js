56fa2948e26d2052deed5e5bd30b3bedccabd221
frenchcdsl44 <chcdsl@yahoo.fr>
2022-02-02 22:41:39 +0100

    removing bootstrap-vue-3

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import App from './App.vue'
 import {createApp} from 'vue'
-import BootstrapVue3 from 'bootstrap-vue-3'
+//import BootstrapVue3 from 'bootstrap-vue-3'
 
 // Optional, since every component import their Bootstrap funcionality
 // the following line is not necessary
 // import 'bootstrap'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+//import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import "bootstrap"
 
 const app = createApp(App)
-app.use(BootstrapVue3)
+//app.use(BootstrapVue3)
 app.mount('#app')
 
 
