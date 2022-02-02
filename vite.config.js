74777f2ae9587d42db49a613595a4bf514667236
frenchcdsl44 <chcdsl@yahoo.fr>
2022-02-02 21:13:22 +0100

    build for demo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+const build = {
+    outDir: 'docs'
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build
 })
+
+
